3cd3c1c16116990438718b3ba18a2ba4e90eb9d2
吴荣华 <wurh@ucweb.com>
2015-02-16 09:16:18 +0800

    [INIT] 初始化

---
+++ music/public/javascripts/index.js
@@ -0,0 +1,26 @@
+function $(s){
+	return document.querySelectorAll(s);
+}
+
+var lis = $("#list li");
+
+for(var i = 0; i< lis.length; i++){
+	lis[i].onclick = function(){
+		for(var j = 0; j< lis.length;j++){
+			lis[j].className = "";
+		}
+		this.className = "selected";
+		load("/media/"+this.title);
+	}
+}
+
+var xhr = new XMLHttpRequest();
+
+function load(url){
+	xhr.open("GET",url);
+	xhr.responseType = "arraybuffer";
+	xhr.onload = function(){
+		console.log(xhr.response);
+	}
+	xhr.send();
+}
