cf1abb493bb976108d530185a2030e0776eb0c7f
吴荣华 <wurh@ucweb.com>
2015-02-16 14:30:34 +0800

    [UPD] 更新音频控制

--- music/public/javascripts/index.js
+++ music/public/javascripts/index.js
@@ -10,17 +10,53 @@ for(var i = 0; i< lis.length; i++){
 			lis[j].className = "";
 		}
 		this.className = "selected";
+		//读取歌曲url
 		load("/media/"+this.title);
 	}
 }
 
+//创建xhr对象
 var xhr = new XMLHttpRequest();
+//创建音频对象
+var ac = new (window.AudioContext||window.webkitAudioContext)();
+//创建音频控制对象
+var gainNode = ac[ac.createGain?"createGain":"createGainNode"]();
+gainNode.connect(ac.destination);
 
+var source = null;
+
+var count = 0;
+
+//加载歌曲资源
 function load(url){
+	var n = ++count;
+	source && source[source.stop?"stop":"noteOff"]();
+	xhr.abort();
 	xhr.open("GET",url);
 	xhr.responseType = "arraybuffer";
 	xhr.onload = function(){
-		console.log(xhr.response);
+		if(n !== count)return;
+		ac.decodeAudioData(xhr.response,function(buffer){
+			if(n !== count)return;
+			var bufferSource = ac.createBufferSource();
+			bufferSource.buffer = buffer;
+			bufferSource.connect(gainNode);
+			bufferSource.connect(ac.destination);
+			bufferSource[bufferSource.start?"start":"noteOn"](0);
+			source = bufferSource;
+		},function(){
+			console.log(error);
+		})
 	}
 	xhr.send();
 }
+
+function changeVolume(percent){
+	gainNode.gain.value = percent * percent;
+}
+
+$("#volume")[0].onchange = function(){
+	changeVolume(this.value/this.max);
+}
+
+$("#volume")[0].onchange();
